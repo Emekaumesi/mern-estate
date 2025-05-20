9dbce5c3c3cf8c2cf163a204a389d76a6ecccf56
emeka <emmyumesi@gmail.com>
2025-05-20 18:17:11 +0100

    first commit baby

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-green-600 font-bold'>Good Morning, My Bloody Ass Neigbhour!</h1>
+      
+    </div>
+  )
+}
